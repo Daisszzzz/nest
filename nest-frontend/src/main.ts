f052425b3133cfef7b406709b850df7bf000bc5c
Shengzong Dai <u7811526@anu.edu.au>
2025-08-11 16:50:30 +1000

    feat: configure Element Plus with English locale and initialize main app modules
    - Import and set Element Plus to use English language pack
    - Register Element Plus icon components globally
    - Import global styles and fonts
    - Initialize and use Pinia, Router, global components, and custom directives

--- nest-frontend/src/main.ts
+++ nest-frontend/src/main.ts
@@ -1,14 +1,34 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
+
+import ElementPlus from 'element-plus'
+// intro English language pack for Element Plus
+import en from 'element-plus/es/locale/lang/en'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'virtual:svg-icons-register'
+import pinia from './store/index'
 import router from './router'
+import registerGlobComp from './components'
+import directives from '@/directives/index'
+
+import 'element-plus/dist/index.css'
+import '@/styles/index.scss'
+import '@/assets/fonts/font.scss'
 
 const app = createApp(App)
 
-app.use(createPinia())
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+
+app.use(ElementPlus, {
+  locale: en,
+})
+
+app.use(registerGlobComp)
+app.use(pinia)
 app.use(router)
+app.use(directives)
 
 app.mount('#app')
